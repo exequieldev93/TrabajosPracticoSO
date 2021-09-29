fef0d6244f20b85991be5c1906d504c331fb345b
exequieldev93 <exequiel191993@gmail.com>
2021-09-29 19:31:46 -0300

    cambios ejercicio 2

--- 1_Tp/Trabajo/ejercicio2.c
+++ 1_Tp/Trabajo/ejercicio2.c
@@ -1,21 +1,3 @@
-/*
-        Escriba un programa que reciba una cantidad variable de nombres de archivo desde la
-        línea de comandos. Por cada nombre de archivo el programa debe crear un proceso hijo
-        que:
-
-        - Imprima su PGID, PPID, PID y el nombre del archivo; y
-        - Use una de las funciones de la familia exec() para ejecutar: wc -l
-        nombre_de_archivo.
-
-        Consideraciones:
-        - Los procesos hijos se deben ejecutar concurrentemente.
-        - El proceso padre debe indicar que proceso hijo va finalizando e indicando de
-        que manera finaliza (Normal o por una señal).
-
-        - Si no se pasa ningún nombre de archivo se debe mostrar un mensaje de error.
-
-        - No se puede utilizar la llamada al sistema system().
-*/
 
 #include <stdio.h>
 #include <unistd.h>
@@ -25,26 +7,24 @@
 
 int main(int argc, char **argv){
     
-    //- Si no se pasa ningún nombre de archivo se debe mostrar un mensaje de error.
+    
     if(argc == 1){
         printf("Faltan archivos por processar");
-        exit(1); //Estamos diciendo que finalizo de manera incorrecta
+        exit(1); 
     }
     int i, estado;
     int proceso;
     for(i=1;i<argc;i++){
-        //Por cada nombre de archivo el programa debe crear un proceso hijo que:
+        
         proceso = fork();
         if(proceso==0){
-        //Imprima su PGID, PPID, PID y el nombre del archivo; y
+        
         printf("PGID: %d, PPID: %d, PID: %d, nombre del archivo: %s \n", getpgid(0), getppid(), getpid(), argv[i]);
-        //Use una de las funciones de la familia exec() para ejecutar: wc -l nombre_de_archivo.
+        
         execlp("wc","wc","-l",argv[i],NULL);
         }
     }
 
-    // El proceso padre debe indicar que proceso hijo va finalizando e indicando de
-    // que manera finaliza (Normal o por una señal).
     for(i=1;i<argc;i++){
         proceso = wait(&estado);
 
